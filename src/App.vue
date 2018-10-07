98cfd81009ddf4a3332ae16eab79139e07abcbe9
kkosuge <root@kksg.net>
2018-10-07 15:55:28 +0900

    feat: add language selection function

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,31 @@
         <div id='editor' ref='editor'></div>
       </div>
     </div>
-    <div class='nav-bar'></div>
+    <div class='nav-bar'>
+      <div class='nav-bar-item'>
+        <select v-model='language' @change='changeLanguage'>
+          <option v-for='lang in languages' :value='lang.value' :key='lang.value'>
+            {{ lang.text }}
+          </option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import * as monaco from 'monaco-editor'
+import languages from './lib/languages'
 import './assets/style.sass'
 
 @Component
 export default class App extends Vue {
   editor?: monaco.editor.IStandaloneCodeEditor
+  editorModel?: monaco.editor.ITextModel
   el?: HTMLElement
+  languages = languages
+  language = 'markdown'
 
   editorOption: monaco.editor.IEditorConstructionOptions = {
     theme: 'vs-dark',
@@ -26,20 +38,30 @@ export default class App extends Vue {
     automaticLayout: true,
     autoIndent: true,
     fontSize: 14,
-    language: 'markdown',
+    language: this.language,
     minimap: {
-      enabled: false,
-    },
+      enabled: false
+    }
   }
 
   mounted() {
     this.el = this.$refs.editor as HTMLElement
     this.editor = monaco.editor.create(this.el, this.editorOption)
+    this.editorModel = monaco.editor.createModel('', this.language)
+    this.editor.setModel(this.editorModel)
+  }
+
+  changeLanguage() {
+    if (this.editorModel) {
+      monaco.editor.setModelLanguage(this.editorModel, this.language)
+    }
   }
 }
 </script>
 
 <style lang='sass'>
+$base-color: #1e1e1e
+$border-color: #454545
 #app
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
@@ -54,7 +76,7 @@ export default class App extends Vue {
   left: 0
   height: 22px
   width: 100%
-  background: #1e1e1e
+  background: $base-color
   -webkit-app-region: drag
 .main
   position: relative
@@ -66,8 +88,8 @@ export default class App extends Vue {
   left: 0
   height: 22px
   width: 100%
-  background: #1e1e1e
-  border-top: 1px solid #454545
+  background: $base-color
+  border-top: 1px solid $border-color
   -webkit-app-region: drag
 .editor-wrapper
   width: 100%
@@ -75,4 +97,20 @@ export default class App extends Vue {
 #editor
   padding-top: 30px
   height: calc(100vh - 84px)
+.nav-bar
+  display: flex
+  justify-content: flex-end
+  font-size: 12px
+select
+  -webkit-appearance: none
+  appearance: none
+  border-radius: 0
+  border: 0
+  margin: 0
+  padding: 2px 4px
+  background: none transparent
+  vertical-align: middle
+  color: $border-color
+  border-radius: 4px
+  box-sizing: content-box
 </style>
---
+++ src/lib/languages.ts
@@ -0,0 +1,23 @@
+const languages = [
+  { text: 'Markdown', value: 'markdown' },
+  { text: 'Plain Text', value: 'text' },
+  { text: 'YAML', value: 'yaml' },
+  { text: 'Batch', value: 'bat' },
+  { text: 'HTML', value: 'html' },
+  { text: 'XML', value: 'xml' },
+  { text: 'Dockerfile', value: 'dockerfile' },
+  { text: 'Javascript', value: 'javascript' },
+  { text: 'TypeScript', value: 'typescript' },
+  { text: 'Ruby', value: 'ruby' },
+  { text: 'Go', value: 'go' },
+  { text: 'CSS', value: 'css' },
+  { text: 'LESS', value: 'less' },
+  { text: 'SCSS', value: 'scss' },
+  { text: 'Solidity', value: 'solidity' },
+  { text: 'MySQL', value: 'mysql' },
+  { text: 'pgSQL', value: 'pgsql' },
+  { text: 'PHP', value: 'php' },
+  { text: 'PowerShell', value: 'powershell' },
+]
+
+export default languages
