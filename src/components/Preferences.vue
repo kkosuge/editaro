85a48eb4ef491ef4db0ff988d552cf3d54e1a9b7
tatat <ioiioioloo@gmail.com>
2018-11-06 21:50:59 +0900

    Vim mode

---
+++ src/components/Preferences.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="preferences" class="preferences">
+    <div class="header">
+      <h1>Preferences</h1>
+      <a href="#" class="close" @click.prevent="close"></a>
+    </div>
+    <div class='pretty p-default p-curve'>
+      <input type='checkbox' id='checkbox' v-model='vimModeEnabled'>
+      <div class='state'>
+        <label for='checkbox'>Vim mode</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import store, { SharedState } from '../store'
+
+@Component
+export default class extends Vue {
+  get vimModeEnabled(): SharedState['vimModeEnabled'] {
+    return this.sharedState.vimModeEnabled
+  }
+
+  set vimModeEnabled(value: SharedState['vimModeEnabled']) {
+    store.commit('vimModeEnabled', value, true)
+  }
+
+  close() {
+    store.commit('showPreferences', false)
+  }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
+
+store.load()
+
+Vue.mixin({
+  data() {
+    return {
+      sharedState: store.state,
+    }
+  },
+})
+
+Vue.sharedState = store.state
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    sharedState: typeof store.state
+  }
+
+  interface VueConstructor {
+    sharedState: typeof store.state
+  }
+}
