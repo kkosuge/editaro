54afecd70db099fb60f89812d6fb63b3bbab598c
kkosuge <root@kksg.net>
2018-11-11 00:51:08 +0900

    show app version in preferences

--- src/background.ts
+++ src/background.ts
@@ -7,13 +7,13 @@ import {
   ipcMain,
   clipboard,
   shell,
-  Menu,
+  Menu
 } from 'electron'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
 import {
   createProtocol,
-  installVueDevtools,
+  installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
 import './updator'
 
@@ -32,7 +32,7 @@ function createMainWindow() {
     width: 600,
     height: 400,
     titleBarStyle: 'hidden',
-    alwaysOnTop: false,
+    alwaysOnTop: false
   })
 
   if (isDevelopment) {
@@ -46,7 +46,7 @@ function createMainWindow() {
       formatUrl({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
-        slashes: true,
+        slashes: true
       })
     )
   }
@@ -80,15 +80,15 @@ function createMainWindow() {
             accelerator: 'CmdOrCtrl+,',
             click: () => {
               if (mainWindow) mainWindow.webContents.send('showPreferences')
-            },
+            }
           },
           {
-            type: 'separator',
+            type: 'separator'
           },
           {
-            role: 'quit',
-          },
-        ],
+            role: 'quit'
+          }
+        ]
       },
       {
         label: 'View',
@@ -98,30 +98,30 @@ function createMainWindow() {
             accelerator: 'Command+R',
             click: function() {
               mainWindow.reload()
-            },
+            }
           },
           {
             label: 'Close',
             accelerator: 'Command+W',
             click: function() {
               mainWindow.close()
-            },
+            }
           },
           {
             label: 'Toggle Full Screen',
             accelerator: 'Ctrl+Command+F',
             click: function() {
               mainWindow.setFullScreen(!mainWindow.isFullScreen())
-            },
+            }
           },
           {
             label: 'Toggle Developer Tools',
             accelerator: 'Alt+Command+I',
             click: function() {
               mainWindow.toggleDevTools()
-            },
-          },
-        ],
+            }
+          }
+        ]
       }
     )
   }
--- src/components/Preferences.vue
+++ src/components/Preferences.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="preferences" class="preferences">
-    <div class="header">
-      <h1>Preferences</h1>
-      <a href="#" class="close" @click.prevent="close"></a>
-    </div>
-    <div class="form-group select-group">
-      <label>Mode:</label>
-      <select v-model='persisted.editorMode'>
-        <option v-for='mode in editorModels' :value='mode.value' :key='mode.value'>
-          {{ mode.text }}
-        </option>
-      </select>
-    </div>
-    <div class="form-group select-group">
-      <label>Theme:</label>
-      <select v-model='persisted.theme'>
-        <option v-for='theme in themes' :value='theme.value' :key='theme.value'>
-          {{ theme.text }}
-        </option>
-      </select>
+    <div class="preferences-inner">
+      <div class="header">
+        <h1>Preferences</h1>
+        <a href="#" class="close" @click.prevent="close"></a>
+      </div>
+      <div class="form-group select-group">
+        <label>Mode:</label>
+        <select v-model='persisted.editorMode'>
+          <option v-for='mode in editorModels' :value='mode.value' :key='mode.value'>
+            {{ mode.text }}
+          </option>
+        </select>
+      </div>
+      <div class="form-group select-group">
+        <label>Theme:</label>
+        <select v-model='persisted.theme'>
+          <option v-for='theme in themes' :value='theme.value' :key='theme.value'>
+            {{ theme.text }}
+          </option>
+        </select>
+      </div>
+      <div class="app-version">エディ太郎: {{ appVersion }}</div>
     </div>
   </div>
 </template>
@@ -32,6 +35,7 @@ import '../lib/theme/vscode'
 import '../lib/theme/dark-grad'
 import '../lib/theme/light-grad'
 import themes from '../lib/theme/themes'
+import { remote } from 'electron'
 
 interface IEditorModes {
   value: IEditorMode
@@ -51,20 +55,10 @@ export default class extends Vue {
     },
   ]
   themes = themes
+  appVersion = remote.app.getVersion()
 
   close() {
     this.memory.showPreferences = false
   }
 }
 </script>
-
-<style lang='scss'>
-.form-group {
-  margin-bottom: 10px;
-}
-.select-group {
-  label {
-    margin-right: 10px;
-  }
-}
-</style>
